29348245c333f3cac3efcef65c0c47bf3b2a7332
rtanubra <rdtanubrata@gmail.com>
2019-03-08 19:35:30 -0500

    merging navigation

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-let navigationBurgerClose = true;
-
-function renderNavigationBurger(){
-    //this function renders navigation burger based on its state
-}
-
-function handleNavigationBurger(){
-    //this function changes the state of the navigation burger upon click.
-    console.log("handling click of navigation burger")
-}
 
 //called at the document ready to listen to events
 function callback(){
-    handleNavigationBurger()
+    handle
 }
 $(callback)
